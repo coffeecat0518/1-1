5e037fe2346b6bf74cbc7cc764ef6105c5e266b4
coffeecat0518 <106022415@live.asia.edu.tw>
2017-12-25 15:20:08 +0800

    1-1

--- src/判斷最大最小值.java
+++ src/判斷最大最小值.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class one {
+public class 判斷最大最小值 {
     public static void main(String[]args){
         Scanner scn=new Scanner(System.in);
         int i,max,min,num;
